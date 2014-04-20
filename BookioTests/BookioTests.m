37b8fbea6f133f25842d0ee4f130d846350c8053
Devashi Tandon <dt2450@columbia.edu>
2014-04-20 05:35:26 -0400

    First version of UI

--- BookioTests/BookioTests.m
+++ BookioTests/BookioTests.m
@@ -2,8 +2,8 @@
 //  BookioTests.m
 //  BookioTests
 //
-//  Created by Pooja Jain on 4/19/14.
-//  Copyright (c) 2014 BookioTeam. All rights reserved.
+//  Created by Devashi Tandon on 4/20/14.
+//  Copyright (c) 2014 Columbia University. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
