e1ba9aedb64ccd6e8abe2f8d7b34e49ef3d4f3f3
Siddharth Boobna <ssb2171@columbia.edu>
2014-04-22 17:26:42 -0400

    added some more queries

--- database/BookioDB/src/SqlAPI.java
+++ database/BookioDB/src/SqlAPI.java
@@ -38,19 +38,33 @@ public class SqlAPI {
 		}
 	}
 	
-	public void insertUser(String user_id, String phone_no) {
+	public void insertUser(String user_id, String fname, String lname, String phone_no) {
 		//createConnectionAndStatement();
-		String query = "insert into User values (\"" + user_id + "\",\"" + phone_no + "\");";
-		try {
-			statement.executeUpdate(query);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		close();
+		String query = "insert into User values (\"" + user_id + "\",\"" + fname + "\",\"" + lname + "\",\"" + phone_no + "\");";
+		insertIntoTable(query);
 	}
 	
 	public String getBooksOfCourse(String course_no) {
-		String query = "select book_name,book_author from CourseBooks natural join Books where course_no=\"" + course_no + "\";";
+		String query = "select ISBN,book_name,book_author from CourseBooks natural join Books where course_no=\"" + course_no + "\";";
+		return getResults(query);
+	}
+	
+	public String getRentAndSellDetails(String isbn) {
+		String query = "select user_id,rent,rent_cost,sell,sell_cost,user_phone from UserBooks natural join User where ISBN=" + isbn + ";";
+		return getResults(query);
+	}
+	
+	public String getMyAccount(String user_id) {
+		String query = "select user_fname,user_lname,user_phone from User where user_id=\"" + user_id + "\";";
+		return getResults(query);
+	}
+	
+	public String getBooksOfUser(String user_id) {
+		String query="select * from (select * from UserBooks where user_id=\"" + user_id + "\") AS temp1 natural join CourseBooks natural join Books natural join Courses;";
+		return getResults(query);
+	}
+	
+	private String getResults(String query) {
 		String result = "";
 		try {
 			resultSet = statement.executeQuery(query);			
@@ -60,6 +74,16 @@ public class SqlAPI {
 		}
 		close();
 		return result;
+		
+	}
+	
+	private void insertIntoTable(String query) {
+		try {
+			statement.executeUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		close();
 	}
 	
 	private String toJSON(ResultSet rs) throws SQLException {
@@ -90,7 +114,7 @@ public class SqlAPI {
 		return results.toString();
 	}
 	
-	public void close() {
+	private void close() {
 		try {
 			if (resultSet != null) {
 				resultSet.close();
@@ -107,4 +131,8 @@ public class SqlAPI {
 
 		}
 	}
+
+	
+
+	
 }
--- database/BookioDB/src/WorkerServlet.java
+++ database/BookioDB/src/WorkerServlet.java
@@ -19,19 +19,36 @@ public class WorkerServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		String queryType = req.getParameter("query");     //takes the query type from the URL
+		String user_id = "";
 		SqlAPI sqlAPI = new SqlAPI();						//instance of the SqlAPI class
 		switch(queryType) {
 			case "insertUser":
-				String user_id = req.getParameter("userid");
+				user_id = req.getParameter("userid");
+				String fname = req.getParameter("fname");
+				String lname = req.getParameter("lname");
 				String phone_no = req.getParameter("phone");
-				sqlAPI.insertUser(user_id, phone_no);
+				sqlAPI.insertUser(user_id, fname, lname, phone_no);
 				break;
 			
 			case "getBooksOfCourse":
 				String course_no = req.getParameter("courseno");
 				resp.getWriter().println(sqlAPI.getBooksOfCourse(course_no));
 				break;
+			
+			case "getRentAndSellDetails":
+				String isbn = req.getParameter("isbn");
+				resp.getWriter().println(sqlAPI.getRentAndSellDetails(isbn));
+				break;
 				
+			case "getMyAccount":
+				user_id = req.getParameter("userid");
+				resp.getWriter().println(sqlAPI.getMyAccount(user_id));
+				break;
+				
+			case "getBooksOfUser":
+				user_id = req.getParameter("userid");
+				resp.getWriter().println(sqlAPI.getBooksOfUser(user_id));
+				break;
 		}
 		
 		
